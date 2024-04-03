89cfcce577776e509567e51d232278048665c4fc
Shivam Wallu <141025670+ShivamWallu@users.noreply.github.com>
2024-04-03 10:57:11 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # source venv/Scripts/activate
-pip install -r requirements.txt
-python3.9 manage.py collectstatic   
+# pip install -r requirements.txt
+# python3.9 manage.py collectstatic   
 
 
 
